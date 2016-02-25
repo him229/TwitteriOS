d078debe394cf127810f47acb06da39ea1a40bef
Himank Yadav <him229@gmail.com>
2016-02-25 03:05:02 -0600

    TweetsViewCotroller modified

--- TwitteriOS/TweetsViewController.swift
+++ TwitteriOS/TweetsViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class TweetsViewController: UIViewController {
-
+    var tweets: [Tweet]!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        TwitterClient.sharedInstance.GET("1.1/statuses/home_timeline.json", parameters: nil, success: { (operation: NSURLSessionDataTask!, response: AnyObject?) -> Void in
+            //print("user: \(response)")
+            
+            let userDictionary = response as! [NSDictionary]
+            
+            let tweets = Tweet.tweetsWithArray(userDictionary)
+            print("\(tweets[0].text)")
+            //print("\(user.name)")
+            
+            
+            
+            
+            }, failure: { (operation: NSURLSessionDataTask?, error: NSError) -> Void in
+                print ("Error")
+        })
+        
         // Do any additional setup after loading the view.
     }
 
