e934678f5eba0c274db6274551e05e4dae9f8e79
Himank Yadav <him229@gmail.com>
2016-03-02 23:21:31 -0600

    working on compose

---
+++ TwitteriOS/ComposeViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ComposeViewController.swift
+//  TwitteriOS
+//
+//  Created by Himank Yadav on 3/2/16.
+//  Copyright © 2016 himankyadav. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var composeTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        composeTextView.delegate = self
+        composeTextView.text = "Placeholder"
+        composeTextView.textColor = UIColor.lightGrayColor()
+        // Do any additional setup after loading the view.
+    }
+    
+    func textViewDidBeginEditing(textView: UITextView) {
+        if textView.textColor == UIColor.lightGrayColor() {
+            textView.text = nil
+            textView.textColor = UIColor.blackColor()
+        }
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Placeholder"
+            textView.textColor = UIColor.lightGrayColor()
+        }
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
